b7db90b473f0381f392d3f5226105d89e63290b7
Michael Lockrey <michael.lockrey@gmail.com>
2015-01-28 00:53:15 +1100

    Create header.php

---
+++ inc/header.php
@@ -0,0 +1,33 @@
+<div class="row">
+    <div class="col-md-offset-1 col-md-10 text-center">
+        <a href="index.php">
+            <span id="logo">
+                <i>
+                    <label id="logoFirst">NOMORE</label><label id="logoCrap">CRAP</label><label id="logoEnd">TIONS</label>
+                </i>
+            </span>
+        </a>
+        <style>
+            #logo {
+                font-family: Verdana, Geneva, sans-serif
+            ;
+                font-size: 45px ;
+                font-weight: bold;
+            }
+            #logoCrap {
+                color:red;
+            }
+            #logoFirst {
+                color : #808080;
+            }
+            #logoEnd {
+                color: black;
+            }
+        </style>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-12">
+        <hr class="low-margin"/>
+    </div>
+</div>
