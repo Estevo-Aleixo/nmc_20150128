55a1cdacf96231a269f7e4f4d5bba06a1cd6893d
Michael Lockrey <michael.lockrey@gmail.com>
2015-01-28 00:31:53 +1100

    Create get_info.php

---
+++ ajax/get_info.php
@@ -0,0 +1,28 @@
+<?php
+require_once("../inc/start.php");
+
+if(!var_check($_GET['id'])) {
+    die(json_encode($error_no_id));
+}
+$id = $_GET['id'];
+
+if(!youtube_validate_id($id)) {
+    die(json_encode($error_invalid_id));
+}
+
+$video = new Youtube($id);
+$info = $video->retrieve_info();
+if($info == False) {
+    die(json_encode(array("error" => True, "value" => $video->info['reason'])));
+}
+if($video->has_asr_caption() == False) {
+    die(json_encode($error_no_asr));
+}
+
+$data = array(
+    "error" => False,
+    "title" => $info['title'],
+);
+
+echo json_encode($data);
+?>
