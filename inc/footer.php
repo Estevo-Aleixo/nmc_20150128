d91d3cc0249c6cbd50aac41e9ce6ba983193ef0a
Michael Lockrey <michael.lockrey@gmail.com>
2015-01-28 00:51:31 +1100

    Create footer.php

---
+++ inc/footer.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+      <div class="container">
+        <p class="text-center">This is a free, open-source project. All <a href="http://goo.gl/forms/lhTtIHUhrt">feedback</a> is welcomed.</p>
+        <p class="text-center">Contact Michael Lockrey via <a href="http://www.twitter.com/mlockrey">Twitter</a></p>
+      </div>
+</footer>
